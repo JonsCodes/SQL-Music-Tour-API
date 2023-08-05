521ff47d6124df7454ba3acba9662e4d049eb803
JonsCodes <johnalyashai@me.com>
2023-08-04 19:16:52 -0700

    added files and minor code

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const {Sequelize} = require('sequelize')
 
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'Zeus208527\*'
+  })
+  
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
 app.get('/', (req, res) => {
