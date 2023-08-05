0ac41337707560d6e0e6d5d91448149145b23ac7
JonsCodes <johnalyashai@me.com>
2023-08-04 19:19:37 -0700

    updated code

--- models/event.js
+++ models/event.js
@@ -1,45 +1,41 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Event extends Model {
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+class Event extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+        // define association here
     }
-  }
-  Event.init({
+}
+Event.init({
     event_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
     },
     date: {
-      type: DataTypes.DATE,
-      allowNull: false
+        type: DataTypes.DATE,
+        allowNull: false
     },
     start_time: {
-      type: DataTypes.TIME,
-      allowNull: false
+        type: DataTypes.TIME,
+        allowNull: false
     },
     end_time: {
-      type: DataTypes.TIME,
-      allowNull: false
+        type: DataTypes.TIME,
+        allowNull: false
     }
-  }, {
+}, {
     sequelize,
     modelName: 'Event',
     tableName: 'event',
     timestamps: false
-  });
-  return Event;
-};
+});
+return Event;
