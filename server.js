def07c53f453f24b06669e7019ca327d921c295f
System Administrator <root@jons-mbp.lan>
2023-08-06 19:59:38 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -12,18 +12,31 @@ const sequelize = new Sequelize({
 });
 
 // CONFIGURATION / MIDDLEWARE
-require("dotenv").config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 
 // ROOT
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the Tour API",
-  });
-});
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// Band Route
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+// Stage Route
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
+
+// Event Route
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
-});
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
